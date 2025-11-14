d0aef5407f05cef6fdcfaddad038fd943c666bf4
Daniel Jones <tortxof@gmail.com>
2025-11-14 02:03:12 -0500

    Add CORS headers to responses for cross-origin requests

--- app.py
+++ app.py
@@ -213,6 +213,12 @@ def load_location():
     g.location = wgs84.latlon(latitude, longitude)
 
 
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
+
+
 @app.get("/time")
 def get_time():
     if os.getenv("OVERRIDE_CURRENT_TIME"):
